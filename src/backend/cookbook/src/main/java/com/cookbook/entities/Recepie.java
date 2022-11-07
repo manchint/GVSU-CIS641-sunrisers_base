3486f5a707336f23541ef7e99945b68237f9011d
manchint <manchint@mail.gvsu.edu>
2022-11-07 12:39:15 -0500

    Search Receipe

---
+++ src/backend/cookbook/src/main/java/com/cookbook/entities/Recepie.java
@@ -0,0 +1,36 @@
+package com.cookbook.entities;
+ipackage com.recepies.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+public class Recepie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long recepieId;
+	private String name;
+	private String description;
+	private String ingredients;
+	private String procedureOfTheRecepie;
+	private Date dateOfPublishing;
+	private String publishedBy;
+	private String recepieURL;
+	@OneToMany(orphanRemoval = true,cascade = CascadeType.ALL )
+	private List<Comment> comments;
+	
+
+}
---
+++ src/backend/cookbook/src/main/java/com/cookbook/utils/RecepieUtils.java
@@ -0,0 +1,102 @@
+package com.cookbook.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.recepies.dao.HomePageDAO;
+import com.recepies.entities.User;
+import com.recepies.model.CommentModel;
+import com.recepies.model.RecepieModel;
+import com.recepies.model.RecepieRequest;
+
+public class RecepieUtils {
+
+
+	/*
+	 * public static List<RecepieModel> buildRecepieModel(HomePageDAO
+	 * homePageDAO,boolean like) { return
+	 * StreamSupport.stream(homePageDAO.getAllRecepies().spliterator(),
+	 * false).collect(Collectors.toList()) .stream().map(recepieEntity
+	 * ->RecepieUtils.buildRecepieModel(recepieEntity, like)
+	 * 
+	 * 
+	 * { RecepieModel recepieModel = new RecepieModel();
+	 * recepieModel.setDateOfPublishing(recepieEntity.getDateOfPublishing());
+	 * recepieModel.setDescription(recepieEntity.getDescription());
+	 * recepieModel.setIngredients(recepieEntity.getIngredients());
+	 * recepieModel.setName(recepieEntity.getName());
+	 * recepieModel.setProcedureOfTheRecepie(recepieEntity.getProcedureOfTheRecepie(
+	 * )); recepieModel.setPublishedBy(recepieEntity.getPublishedBy());
+	 * recepieModel.setRecepieId(recepieEntity.getRecepieId());
+	 * recepieModel.setRecepieURL(recepieEntity.getRecepieURL());
+	 * recepieModel.setCommentModels(recepieEntity.getComments().stream()
+	 * .map(comment ->
+	 * RecepieUtils.buildCommentModel(comment)).collect(Collectors.toList()));
+	 * 
+	 * return recepieModel;
+	 * 
+	 * }).collect(Collectors.toList()); }
+	 */
+
+	public static com.recepies.entities.Recepie buildRecepieEntity(String uri, RecepieRequest request) {
+		com.recepies.entities.Recepie recepie = new com.recepies.entities.Recepie();
+		recepie.setDateOfPublishing(new Date());
+		recepie.setDescription(request.getDescription());
+		recepie.setIngredients(request.getIngredients());
+		recepie.setName(request.getName());
+		recepie.setPublishedBy(request.getPublishedBy());
+		recepie.setProcedureOfTheRecepie(request.getProcedureOfTheRecepie());
+		recepie.setRecepieURL(uri);
+		return recepie;
+
+	}
+
+	public static com.recepies.model.RecepieModel buildRecepieModel(com.recepies.entities.Recepie recepieEntity,
+			boolean like) {
+		com.recepies.model.RecepieModel recepieModel = new com.recepies.model.RecepieModel();
+		recepieModel.setRecepieId(recepieEntity.getRecepieId());
+		recepieModel.setDateOfPublishing(new Date());
+		recepieModel.setDescription(recepieEntity.getDescription());
+		recepieModel.setIngredients(recepieEntity.getIngredients());
+		recepieModel.setName(recepieEntity.getName());
+		recepieModel.setPublishedBy(recepieEntity.getPublishedBy());
+		recepieModel.setProcedureOfTheRecepie(recepieEntity.getProcedureOfTheRecepie());
+		recepieModel.setRecepieURL(recepieEntity.getRecepieURL());
+		recepieModel.setCommentModels(recepieEntity.getComments() != null ? recepieEntity.getComments().stream()
+				.map(x -> RecepieUtils.buildCommentModel(x)).collect(Collectors.toList()) : new ArrayList<>());
+		recepieModel.setWheatherLikedByuser(like);
+		return recepieModel;
+
+	}
+
+	public static com.recepies.model.CommentModel buildCommentModel(com.recepies.entities.Comment commentEntity) {
+		CommentModel commentModel = new CommentModel();
+		commentModel.setCommentedContent(commentEntity.getCommentedContent());
+		commentModel.setPostedBy(commentEntity.getPostedBy());
+		commentModel.setPostedOnRecepie(commentEntity.getRecepie().getRecepieId());
+		return commentModel;
+	}
+
+	public static com.recepies.entities.Comment buildCommentEntity(CommentModel commentModel,
+			com.recepies.entities.Recepie recepie) {
+		com.recepies.entities.Comment commentEntity = new com.recepies.entities.Comment();
+		commentEntity.setCommentedContent(commentModel.getCommentedContent());
+		commentEntity.setPostedBy(commentModel.getPostedBy());
+		commentEntity.setRecepie(recepie);
+		return commentEntity;
+	}
+
+}
