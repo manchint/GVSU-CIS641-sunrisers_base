2c02ad4afbb5b04891f889f5509395bf767454a3
ashutoast7 <112903683+ashutoast7@users.noreply.github.com>
2022-12-06 18:08:16 -0500

    Add files via upload

---
+++ src/frontend/cookbook/src/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setUserDetails } from '../redux/user.js'
+import { useSelector } from 'react-redux';
+
+function Login() {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    
+    const dispatch = useDispatch();
+    let navigate = useNavigate();
+
+    const user = useSelector(state =>  state.user.user)
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*"
+      }
+    async function onClickLogin(){
+        navigate('/');
+        
+        
+        // const data = await axios.post('http://localhost:3002/signin',{
+        //     "email": email,
+        //     "password": password
+        // }, headers).then (res => {
+        //     console.log(res)
+        //     dispatch(setUserDetails(res.data));
+        //     navigate('/');
+        // });
+
+    }
+
+    return (
+        <div className='login-form'>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputUsername" className="form-label">Email</label>
+                    <input type="text" className="form-control" id="exampleInputUsername" 
+                        onChange={(e) => setEmail(e.target.value)}
+                        value = {email} />
+                </div>               
+                <div className="mb-3">
+                    <label for="exampleInputCPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputCPassword" 
+                        onChange={(e) => setPassword(e.target.value)}
+                        value = {password}/>
+                </div>
+                <button type="submit" className="btn btn-primary f-right" 
+                    onClick={() => onClickLogin()}>Login</button>
+            </form>
+        </div>
+
+    )
+}
+
+export default Login
