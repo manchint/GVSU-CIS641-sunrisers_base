926c7b5059f9a55d36b115ca01b8b35c6399f650
manchint <manchint@mail.gvsu.edu>
2022-12-06 19:20:56 -0500

    fixes

--- src/frontend/cookbook/src/CardComponent/CardComponent.js
+++ src/frontend/cookbook/src/CardComponent/CardComponent.js
@@ -2,25 +2,22 @@ import React from 'react';
 import './CardComponent.css';
 import { useNavigate } from 'react-router-dom'
 
+import item1 from '../../src/Assets/images/item1.jpeg';
+import item2 from '../../src/Assets/images/item2.webp';
+import item3 from '../../src/Assets/images/item3.jpeg';
+import item4 from '../../src/Assets/images/item4.jpeg';
+import item5 from '../../src/Assets/images/item5.jpeg';
+
 function CardComponent(props) {
     let navigate = useNavigate();
 
-    const navigateTo = (item) =>{
-        navigate('/'+item)
-    }
+    const images = [item1, item2, item3, item4, item5]
     return (
-        <div className="card" onClick={() => navigateTo('viewdetails')}>
-            <div className='item-img'></div>
-            {/* <img src='../chicke-tikka.jpg' className="card-img-top" alt="tikka" /> */}
+        <div className="card" onClick={() => navigate('/viewdetails', {state:{id:props.id}})}>
+                <img src= {images[props.id-1]}/>
                 <div className="card-body">
-                    <h5 className="card-title">{props.name}</h5>  
-                    <div className='mt-10'> 
-                        <span class="fa fa-star mr-10 checked"></span>
-                        <span class="fa fa-star mr-10 checked"></span>
-                        <span class="fa fa-star mr-10 checked"></span>
-                        <span class="fa fa-star mr-10"></span>
-                        <span class="fa fa-star mr-10"></span> 
-                    </div>
+                    <h5 className="card-title">{props.name}</h5>
+                    <div><i className='icon-like'></i> <span className='like-count'>231</span></div>
                 </div>                               
         </div>
     )
